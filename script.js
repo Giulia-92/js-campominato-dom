be83ac7337539407a224545b10b3263c823b10a3
Giulia-92 <cionco.giulia@gmail.com>
2022-04-05 16:11:18 +0200

    js-campominato-dom

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("play").addEventListener("click", function(){
+   let livello = play();
+   const griglia = document.getElementById("main");
+   main.innerHTML = "";
+    let testo = 1;
+    for (let numrow=1; numrow<=livello; numrow++){
+    const contenuto = document.createElement("div");
+    contenuto.setAttribute("class", "row");
+    griglia.append(contenuto);
+    for (let numero = 1; numero<=livello; numero++) {
+        const colonna = document.createElement("div");
+        colonna.setAttribute("class", "col-1");
+        contenuto.append(colonna);
+        colonna.append(testo);
+        testo++;
+    }
+}
+
+})
+
+function play(){
+console.log("play");
+let valore = document.getElementById("livelli").value;
+return valore;
+}
+
+
+
+
+//document.querySelector("col-1").addEventListener("click", function(){
+ 
+//})
+
+
+//function color(){
+    console.log ("colore")
+//}
+
+
+
+
+
+
